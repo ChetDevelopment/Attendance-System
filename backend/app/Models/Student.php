6288e7bb75ad213ca941dff89dfdcc397642e75b
Serey002 <serey.phem.12022006@gmail.com>
2026-02-27 14:05:58 +0700

    Create students table

---
+++ backend/app/Models/Student.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Classes;
+
+class Student extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'student_code',
+        'first_name',
+        'last_name',
+        'email',
+        'class_id',
+        'qr_code',
+        'face_image',
+        'is_active',
+    ];
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+}

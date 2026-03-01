df099d0e2b6911f6dd2c7d03878f84b8746ecc26
Serey002 <serey.phem.12022006@gmail.com>
2026-03-01 16:12:56 +0700

    create seeder academic year and insert data

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            SessionSeeder::class
+            SessionSeeder::class,
+            AcademicYearSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 

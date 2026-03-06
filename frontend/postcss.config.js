33acdd301b2172948cb8968ec9ea57bd829a56b0
SREYROTH.SANG <SREYROTH.SANG@PNC.LOCAL>
2026-03-06 15:45:50 +0700

    feat:develop admin management apis

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -3,3 +3,17 @@ export default {
     '@tailwindcss/postcss': {},
   },
 };
+axios.get("http://127.0.0.1:8000/api/dashboard/present-today")
+.then(response => {
+    this.presentToday = response.data.data
+})
+
+axios.get("http://127.0.0.1:8000/api/dashboard/absent-today")
+.then(response => {
+    this.absentToday = response.data.data
+})
+
+axios.get("http://127.0.0.1:8000/api/dashboard/late-today")
+.then(response => {
+    this.lateToday = response.data.data
+})
